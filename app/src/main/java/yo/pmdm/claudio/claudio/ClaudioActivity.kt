74e02ad10c5efdc3866cc2942e1f7d813c1e6bae
DESKTOP-KRVA0C7\Claudio <claudiochamorro61@gmail.com>
2018-09-25 18:35:33 +0200

    Initial commit

---
+++ app/src/main/java/yo/pmdm/claudio/claudio/ClaudioActivity.kt
@@ -0,0 +1,12 @@
+package yo.pmdm.claudio.claudio
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ClaudioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_claudio)
+    }
+}
